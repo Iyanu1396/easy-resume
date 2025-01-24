4c7499bc80a2ca997a1cdba544b248d466eaa820
IyanuCodes <108562866+Iyanu1396@users.noreply.github.com>
2025-01-24 18:17:11 +0100

    Iyanu/changes (#2)
    * improved typography with fonts and add supabase config files
    
    * set up login auth
    
    * commit

---
+++ src/lib/services/apiAuth.ts
@@ -0,0 +1,45 @@
+import  supabase  from "../supabase";
+
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
+interface SignUpParams {
+  fullName: string;
+  email: string;
+  password: string;
+}
+
+export async function Login({ email, password }: LoginParams) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
+export async function signUp({ fullName, email, password }: SignUpParams) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
+
+export async function logOut() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
+}
